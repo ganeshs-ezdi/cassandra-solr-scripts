113036ff077554a83158e315bad7d20863fdce76
ganeshs-ezdi <ganesh.s@ezdi.us>
2017-05-12 18:16:51 +0530

    Added SOLR downconfig script

---
+++ solr-downconfig.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+SOLR_PATH=/opt/solr
+SOLR_COULD_SCRIPT_PATH=$SOLR_PATH/server/scripts/cloud_scripts
+ZKHOST=$1
+CONFDIR=$2
+
+CONFIGS="CCPhysician DocumentMaster MacrosSuggester MtDocumentMaster RefDocumentSearchh CNTDocumentMaster NewNPDocumentMaster SpellCheck UserMaster AdtSearch"
+
+for $CONFNAME in $CONFIGS; do
+    $SOLR_COULD_SCRIPT_PATH/zkcli.sh -cmd downconfig -zkhost $ZKHOST -confname $CONFNAME -d $CONFDIR/$CONFNAME
+done
